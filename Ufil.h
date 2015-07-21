fce5107286425ff2881676844a00cf86bc5b4267
brajard <julien.brajard@inria.fr>
2015-07-21 16:22:07 +0200

    modified Hfil,Ufil,Vfil init to prepare geostrophic init

--- Ufil.h
+++ Ufil.h
@@ -1,18 +1,24 @@
 forward(YREAL a, YREAL b, YREAL c)
 {
-	if(Yt <= 2)
-	  YS1 = a ;
-	else YS1=b+alpha*(a-2*b+c);
+  if (Yt == 1)
+    YS1 = c ;
+  else
+    if(Yt == 2)
+      YS1 = a ;
+    else YS1=b+alpha*(a-2*b+c);
 }
 
 backward(YREAL a, YREAL b, YREAL c)
 {
-	if(Yt<=2)
-	  YJ1I1 = 1;
-	else
-	{
-	  YJ1I1 = alpha;
-	  YJ1I2 = 1 - 2*alpha;
-	  YJ1I3 = alpha;
-	}  
+  if (Yt == 1)
+    YJ1I3 = 1;
+  else
+    if(Yt == 2)
+      YJ1I1 = 1;
+    else
+      {
+	YJ1I1 = alpha;
+	YJ1I2 = 1 - 2*alpha;
+	YJ1I3 = alpha;
+      }  
 }
