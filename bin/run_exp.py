7af7997b4cb978d367f43c10f5e6bc44fba35dbf
brajard <julien.brajard@inria.fr>
2016-02-10 17:29:43 +0100

    add namelist in python scirpt

--- bin/run_exp.py
+++ bin/run_exp.py
@@ -11,20 +11,31 @@ cdir = os.getcwd()
 datadir =  os.path.abspath(os.path.join (bindir, "../data"))
 scriptdir =  os.path.abspath(os.path.join (bindir, "../scripts"))
 sys.path.append(srcdir)
-from mytools import makedirs_sure, silentremove
+from mytools import makedirs_sure, silentremove, make_namelist
 exname = 'shalw'
 exefile = os.path.join(os.path.abspath(bindir), exname)
 
 #global parameters
+exp_name = '00'
+expdir = os.path.join(datadir,'EXP' + exp_name)
 
 yao_opt = {
 'compile':'True', 
-'gen_opt':'-p +O3 -x '+exefile,
+'gen_opt':'-x '+exefile,
 'forw': os.path.join(scriptdir,'forw.i')
 }
 
-exp_name = '00'
-expdir = os.path.join(datadir,'EXP' + exp_name)
+namelist = {
+'indir':os.path.join(expdir,"input"),
+'outdir':os.path.join(expdir,"output"),
+'obsfile':'obs.dat',
+'bck_state':'snapshot_10.nc',
+'out_true':'state_true.nc',
+'out_obs':'obs_per.dat',
+'obsper':'1',
+'bckper':'10',
+}
+
 
 
 #print cdir
@@ -34,8 +45,8 @@ print expdir
 
 #prepare paths
 makedirs_sure(expdir)
-indir = os.path.join(expdir,"input")
-outdir = os.path.join(expdir,"output")
+indir = namelist['indir']
+outdir = namelist['outdir']
 makedirs_sure(indir)
 makedirs_sure(outdir)
 
@@ -53,9 +64,14 @@ if yao_opt['compile']:
         ['cd ' + srcdir + ' ; ../bin/yao92 ' + yao_opt["gen_opt"] + ' shalw ; cd ' + cdir],
         shell=True)
 
+
+#GENERATION OF NAMELIS
+namefile = os.path.join(indir,"namelist")
+make_namelist(namefile,namelist)
+
 #EXECUTE THE PROJECT
 if os.path.isfile(exefile):
-    namefile = os.path.join(indir,"namelist")
+    
     strcmd = [exefile + ' namelist ' + namefile + ' -i ' + yao_opt["forw"] ]
     print strcmd
     sp.call(
