31941e0ce45b4dc9462344073060fdd7d23f72c2
brajard <julien.brajard@upmc.fr>
2017-01-31 09:55:12 +0100

    new diags function

--- scripts/make_diag.py
+++ scripts/make_diag.py
@@ -53,6 +53,99 @@ def load_exp(dirname,Nens,true_file='state_true_0.nc',pref_ret='state_ret_',prod
         fx.close()
     return(xa,xt)
 
+def Brier_score(xt,xa,thr,nbins=20,tmask=-1):
+    #True (obs) field
+    xxt = np.reshape(xt[tmask,:,:],-1)
+        
+    #Analyse (forecast) fiels
+    xxa = [np.reshape(xaj[tmask,:,:],-1) for xaj in xa]
+    
+    #Ensemble size
+    nens = len(xxa)
+    
+    #Number of forecast
+    N = xxt.size
+    
+    #Forecast
+    fcast = np.zeros(N)
+    
+    for i in range(N):
+        F = np.array([x[i] for x in xxa])
+        fcast[i] = np.sum(F>thr)/float(nens)
+    
+    # forecast bins
+    fp_bins = np.linspace(0,1,nbins+1)
+    fp_bins[-1]=1.001
+
+    #forecast probabilities
+    fp = np.zeros(nbins)
+    
+    #observed frequency
+    obsf = np.zeros(nbins)
+    
+    # Number of time fp was predicted
+    n = np.zeros(nbins)
+    
+    #observed climatology
+    obsc = np.sum(xxt>thr)
+
+    for i in range(nbins):
+        ind, = np.nonzero(np.logical_and(fcast>=fp_bins[i],fcast<fp_bins[i+1]))
+        n[i] = ind.size
+        if n[i] > 0 :
+            obsf[i] = np.sum(xxt[ind]>thr)/float(n[i])
+            fp[i] = np.mean(fcast[ind])
+    
+    #Brier Score
+
+    #UNC
+    if obsc==0 or obsc==1:
+        UNC = 0.0
+    else:
+        UNC = (obsc/float(N))*(1-obsc/float(N))
+    
+    #REL
+    REL = np.sum(n*((fp-obsf)**2))/float(N)
+
+    #REF (Resolution in Talagrand)
+    REF = np.sum(n*obsf*(1-obsf))/float(N)
+
+    return REL,REF,UNC
+
+
+    
+    
+def reliability_fast(xt,xa,thr,nbins=20,tmask=-1,oplot=True):
+    #CHANGE : xa is a list of arrays (or netcdf.Variable type)
+    if mask is None:
+        mask = np.ones(xt.shape,dtype=bool)
+        
+    #True (obs) field
+    xxt = np.reshape(xt[tmask,:,:],-1)
+        
+    #Analyse (forecast) field
+    xxa = [xj[tmask,:,:] for xj in xa]
+        
+    #Ensemble size
+    nens = xxa.size
+
+    #Total number of forecast
+                  
+
+    #Analyse (forecast) probability
+    fp = np.zeros(len(xxt))
+    fp = np.arrays([np.count_nonzero(xxa[:,i]>thr)/nens for i in range(len(xxt))] )
+    for i in range(len(xxt)):
+        fp[i] = np.count_nonzero(xxa[:,i]>thr)
+    fp = fp/nens
+    freq_tot = np.mean(fp)
+    xprob = np.linspace(0,1,nbins+1)
+    #xc = 0.5*(xprob[:-1]+xprob[1:])
+    xc = np.zeros(nbins)
+    xprob[-1]=1.1 #trick to allow strict inequality
+    obsf = np.zeros(nbins)
+    Np = np.zeros(nbins)
+
 def reliability(xt,xa,thr,nbins=20,mask=None,oplot=True):
     if mask is None:
         mask = np.ones(xt.shape,dtype=bool)
@@ -97,6 +190,31 @@ def reliability(xt,xa,thr,nbins=20,mask=None,oplot=True):
     return(obsf,xc,Np)
         
    
+def rank_diag_fast (xt,xa,hinit=None,tmask=-1):
+    #CHANGE : xa is a list of arrays (or netcdf.Variable type)
+
+    Nens = len(xa)
+    if hinit is None:
+        hinit = np.zeros(Nens+1)
+    
+#    N = np.sum(mask)
+    xxa = [np.reshape(xaj[tmask,:,:],-1) for xaj in xa]
+
+  #  ninf = np.Inf*np.ones([1,xxa.shape[1]])
+  #  nninf = np.NINF*np.ones([1,xxa.shape[1]])
+  #  xxa = np.concatenate((nninf,xxa,ninf))
+    xxt = np.reshape(xt[tmask,:,:],-1)
+
+    print('Number of analysis (forecast)',xxt.size)
+    for i in range(len(xxt)):
+        l = np.array([x[i] for x in xxa])
+  #      l.sort()
+        ind = np.sum(l>xxt[i])
+#        ind = np.searchsorted(l,xxt[i])
+        hinit[ind]+=1
+    
+    return(hinit)
+
 
 def rank_diag(xt,xa,hinit=None,mask=None):
     Nens = xa.shape[0]
